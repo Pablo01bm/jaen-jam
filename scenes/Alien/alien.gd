e318cbf464d63149e94b0b0c96cda6dcb7353143
Pablo <pablobormeg@gmail.com>
2026-07-25 13:16:00 +0200

    Added level logic and bad guy logic and kill alien logic

--- scenes/Alien/alien.gd
+++ scenes/Alien/alien.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
+class_name Alien
+
+signal died(alien: Alien)
 
 var SPEED_X = RandomNumberGenerator.new()
 var SPEED_Y = RandomNumberGenerator.new()
 var RANDOM = RandomNumberGenerator.new()
+
+var is_hijoputa: bool = false
 
 @onready var face = %AlienFace
  
@@ -28,3 +33,23 @@ func get_info_array():
 
 func reroll_features():
 	face.randomize_face()
+
+func set_hijoputa(value: bool) -> void:
+	is_hijoputa = value
+
+func get_face_signature() -> Array:
+	var signature = []
+	for value in face.attributes:
+		signature.append(int(value))
+	return signature
+
+func has_same_face_as(other: Alien) -> bool:
+	return get_face_signature() == other.get_face_signature()
+
+# Llamar desde el sistema de disparo cuando el alien recibe un impacto
+func receive_hit() -> void:
+	die()
+
+func die() -> void:
+	died.emit(self)
+	queue_free()
