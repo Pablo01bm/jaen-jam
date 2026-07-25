aff7f7a4ab363f5000143d9eed2762c0a692a04a
Pablo <pablomillancb@gmail.com>
2026-07-25 09:03:00 +0200

    change window keep

--- scenes/Alien/alien.gd
+++ scenes/Alien/alien.gd
@@ -20,7 +20,6 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	
 	if collision:
-		print("HOLA")
 		velocity = velocity.bounce(collision.get_normal().rotated(RANDOM.randf_range(-10.0, 10.0)))
 		
 
