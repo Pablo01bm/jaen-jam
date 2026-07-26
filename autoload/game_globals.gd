347f5a3650dc0a6bc439c633cbd3b6d6262d6f5f
Pablo <pablomillancb@gmail.com>
2026-07-26 13:14:20 +0200

    ending screen

--- autoload/game_globals.gd
+++ autoload/game_globals.gd
@@ -31,7 +31,30 @@ signal level_started
 signal level_finished
 
 signal boss_appeared
+
+var high_score: float = 30.0
  
+func _ready() -> void:
+	var hs: HighScore = ResourceLoader.load("user://high-score.tres", "", ResourceLoader.CACHE_MODE_IGNORE)
+	if hs != null:
+		print("load record")
+		print(hs.score)
+		high_score = hs.score
+
+
+func save_record(face: AlienFace):
+	if score < high_score and score > 0.0:
+		high_score = score
+		var hs = HighScore.new()
+		hs.score = score
+		hs.face = face.attributes
+		ResourceSaver.save(hs, "user://high-score.tres")
+		print("score saved")
+
+func get_high_score_face():
+	var hs: HighScore = ResourceLoader.load("user://high-score.tres", "", ResourceLoader.CACHE_MODE_IGNORE)
+	return hs.face
+
 func reset() -> void:
 	current_level_index = 0
 	score = 0
---
+++ data/high_score.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name HighScore
+
+@export var score: float = 30.0
+@export var face: Array = [0, 0, 0, 0, 0, 0]
