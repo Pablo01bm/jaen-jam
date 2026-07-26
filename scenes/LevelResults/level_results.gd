8636cb774cf22e7c18515589c15fe07fba474aa7
Pablo <pablomillancb@gmail.com>
2026-07-26 14:28:20 +0200

    crime shows

--- scenes/LevelResults/level_results.gd
+++ scenes/LevelResults/level_results.gd
@@ -14,29 +14,19 @@ const ALIEN_FACE_SCENE := preload("res://scenes/alien_face/AlienFace.tscn")
 
 func _ready() -> void:
 	_fill_soab_aliens_faces()
+	GameGlobals.score = snapped(GameGlobals.score, 0.1)
+	$Time.text = str(GameGlobals.score)
 
 
 func _fill_soab_aliens_faces() -> void:
 	var defeated: Array = GameGlobals.alien_motherfuckers_faces
 
 	for i in range(defeated.size()):
-		_add_result_row(i, defeated[i])
+		$Aliens.get_child(i).update_face(defeated[i])
+		$Aliens.get_child(i).visible = true
+		$Aliens.get_child(i).get_child(7).text = CrimeGenerator.generate_crime(lang)
 	
 	GameGlobals.alien_motherfuckers_faces.clear()
-
-
-func _add_result_row(index: int, face_signature: Array) -> void:
-	var row_y: float = start_y + index * row_height
-
-	var face: Node2D = ALIEN_FACE_SCENE.instantiate()
-	add_child(face)
-	face.position = Vector2(face_offset_x, row_y)
-	face.update_face(face_signature)
-
-	var label := Label.new()
-	add_child(label)
-	label.position = Vector2(label_offset_x, row_y)
-	label.text = CrimeGenerator.generate_crime(lang)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
